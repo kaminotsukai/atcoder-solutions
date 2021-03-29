1eaa2b3876e1083749b58b5faea33c2234ca20f8
kaminotsukai <japanbritish.1107@gmail.com>
2021-03-30 02:11:49 +0900

    feat: abc130_a

---
+++ abc_a/abc130_a.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types = 1);
+
+$ints = fn () => array_map('intval', explode(' ', trim(fgets(STDIN))));
+$strs = fn () => array_map('strval', explode(' ', trim(fgets(STDIN))));
+function printn($value) {
+    echo $value . PHP_EOL;
+}
+
+[$x, $a] = $ints();
+
+$x >= $a
+    ? printn(10)
+    : printn(0);
