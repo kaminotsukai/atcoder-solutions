392aeccff4e3fc008be3ace406719aadfcce16e7
kaminotsukai <japanbritish.1107@gmail.com>
2021-03-30 01:26:46 +0900

    feat: abc122_a

--- abc_a/abc122_a.php
+++ abc_a/abc122_a.php
@@ -2,6 +2,13 @@
 
 $ints = fn () => array_map('intval', explode(' ', trim(fgets(STDIN))));
 $strs = fn () => array_map('strval', explode(' ', trim(fgets(STDIN))));
+
 function printn($value) {
     echo $value . PHP_EOL;
 }
+
+[$b] = $strs();
+
+$pairs = ['A' => 'T', 'T' => 'A', 'C' => 'G', 'G' => 'C'];
+
+printn($pairs[$b]);
