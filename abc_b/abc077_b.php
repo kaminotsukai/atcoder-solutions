ff03971879622e09209b5f96c6374b6660747aa9
kaminotsukai <japanbritish.1107@gmail.com>
2021-04-04 02:39:03 +0900

    feat: abc077_b

---
+++ abc_b/abc077_b.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types = 1);
+
+$ints = fn () => array_map('intval', explode(' ', trim(fgets(STDIN))));
+$strs = fn () => array_map('strval', explode(' ', trim(fgets(STDIN))));
+function printn($value) {
+    echo $value . PHP_EOL;
+}
+
+[$n] = $ints();
+
+echo (floor(sqrt($n)) ** 2) . PHP_EOL;
