22996492980feb03d3c8396bab51b649b30b1bf6
kaminotsukai <japanbritish.1107@gmail.com>
2021-04-06 23:48:26 +0900

    feat: abc161_a

---
+++ abc_a/abc161_a.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types = 1);
+
+$ints = fn () => array_map('intval', explode(' ', trim(fgets(STDIN))));
+$strs = fn () => array_map('strval', explode(' ', trim(fgets(STDIN))));
+$printn = fn ($value) => print($value . PHP_EOL);
+
+[$X, $Y, $Z] = $ints();
+
+echo implode(' ', [$Z, $X, $Y]) . PHP_EOL;
