d6f4f53a06482c2b2eeae922feb7ebf327a15368
kaminotsukai <japanbritish.1107@gmail.com>
2021-04-09 14:33:26 +0900

    feat: a問題達

---
+++ abc_a/abc170_a.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types = 1);
+
+$ints = fn () => array_map('intval', explode(' ', trim(fgets(STDIN))));
+$strs = fn () => array_map('strval', explode(' ', trim(fgets(STDIN))));
+$printn = fn ($value) => print($value . PHP_EOL);
+
+$lists = $ints();
+echo array_search(0, $lists) + 1 . PHP_EOL;
