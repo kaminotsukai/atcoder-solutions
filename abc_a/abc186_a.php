c27f11912f6b36ada87056cd570c1dab3310b436
kaminotsukai <japanbritish.1107@gmail.com>
2021-04-11 15:29:40 +0900

    feat: 2021-4-11 A問題 * 3

---
+++ abc_a/abc186_a.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types = 1);
+
+$ints = fn () => array_map('intval', explode(' ', trim(fgets(STDIN))));
+$strs = fn () => array_map('strval', explode(' ', trim(fgets(STDIN))));
+$printn = fn ($value) => print($value . PHP_EOL);
+
+[$N, $W] = $ints();
+
+echo intdiv($N, $W) . PHP_EOL;
