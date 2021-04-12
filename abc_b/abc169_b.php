97579cb88da621d94cce24c7105ebea428ef70e8
kaminotsukai <japanbritish.1107@gmail.com>
2021-04-12 20:16:03 +0900

    fix: abc169_b 改良

--- abc_b/abc169_b.php
+++ abc_b/abc169_b.php
@@ -14,3 +14,29 @@ for ($i = 0; $i < $N; $i ++) {
 
 echo $sum > pow(10, 18) ? -1 : (int) $sum;
 echo PHP_EOL;
+
+
+/**
+ * 解説
+ * 0が一つでも含まれると全て0になる
+ * PHPは10**18まで扱えるのでそれを超えると正確な判定ができなくなる
+ */
+
+// [$N] = $ints();
+// $Ai = $ints();
+
+// if (in_array(0, $Ai, true)) {
+//     echo 0 . PHP_EOL;
+//     exit;
+// }
+
+// $sum = 1;
+// for ($i = 0; $i < $N; $i ++) {
+//     $sum *= $Ai[$i];
+//     if ($sum > pow(10, 18)) {
+//         echo -1 . PHP_EOL;
+//         exit;
+//     }
+// }
+
+// echo $sum . PHP_EOL;
