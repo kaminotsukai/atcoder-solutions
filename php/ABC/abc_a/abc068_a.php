da2b5bb305115321e02ed07624c4f8b03b258851
kaminotsukai <japanbritish.1107@gmail.com>
2021-06-08 22:34:20 +0900

    a問題

---
+++ php/ABC/abc_a/abc068_a.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types = 1);
+
+$ints = fn () => array_map('intval', explode(' ', trim(fgets(STDIN))));
+$strs = fn () => array_map('strval', explode(' ', trim(fgets(STDIN))));
+
+[$s] = $strs();
+echo 'ABC' . $s . PHP_EOL;
