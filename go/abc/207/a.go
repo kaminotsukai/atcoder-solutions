fc142ec8b6a3afd3fe131355d5a091ec4910c53b
kaminotsukai <japanbritish.1107@gmail.com>
2021-10-29 15:31:36 +0900

    abc207 改善(go)

--- go/abc/207/a.go
+++ go/abc/207/a.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func main() {
-	slice := make([]int, 3)
-	fmt.Scanln(&slice[0], &slice[1], &slice[2])
+	var a, b, c int
+	fmt.Scan(&a, &b, &c)
 
-	sort.Ints(slice)
-	fmt.Println(slice[1] + slice[2])
+	// 3つの中から大きいもの2つの合計 = 3つの合計から最も小さい値を引いたもの
+	fmt.Println(a + b + c - min(a, b, c))
+}
+
+
+func min(a ...int) int {
+	x := 1<<61 - 1
+	for i := 0; i < len(a); i++ {
+		if a[i] < x {
+			x = a[i]
+		}
+	}
+	return x
 }
